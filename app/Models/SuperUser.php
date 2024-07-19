7b10407d326b1a179fb87dec8bf0225c91f8e806
JunarisAlf <junaris0715@gmail.com>
2024-07-19 20:32:28 +0700

    fix (panel): update superUser and user Modal to implement FilamentUser

--- app/Models/SuperUser.php
+++ app/Models/SuperUser.php
@@ -2,14 +2,21 @@
 
 namespace App\Models;
 
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class SuperUser extends Authenticatable
+class SuperUser extends Authenticatable implements FilamentUser
 {
     use HasFactory, Notifiable;
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return true;
+    }
+
      /**
      * The attributes that are mass assignable.
      *
