b9b1baf3c4ae9c5e382a6ef5483c4954c1de7cba
JunarisAlf <junaris0715@gmail.com>
2024-09-09 15:34:07 +0700

    feat (enum): update EmployeeTypeEnum

--- app/Enums/EmployeeTypeEnum.php
+++ app/Enums/EmployeeTypeEnum.php
@@ -7,6 +7,16 @@ use App\Trait\EnumHelper;
 enum EmployeeTypeEnum: string
 {
     use EnumHelper;
+    case OWNER      = 'Pemilik';
     case BARBER     = 'Barber';
     case CASHIER    = 'Kasir';
+
+    public function getColor(): string
+    {
+        return  match ($this) {
+            self::OWNER     => 'danger',
+            self::BARBER    => 'success',
+            self::CASHIER   => 'primary',
+        };
+    }
 }
